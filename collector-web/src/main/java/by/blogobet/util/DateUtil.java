5437055bee00c4419e96c8f98ab3d0d58384e262
u6remix <pochtaremix@gmail.com>
2017-03-01 21:41:54 +0300

    test commit in test branch

--- collector-web/src/main/java/by/blogobet/util/DateUtil.java
+++ collector-web/src/main/java/by/blogobet/util/DateUtil.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class DateUtil {
 
-	//test 123
+	//test 123 in test branch
 	public static Date getDate2HoursBefore(){
 		long time2HourBefore = new Date().getTime() - 2*60*60*1000;
 		Date date = new Date(time2HourBefore);
