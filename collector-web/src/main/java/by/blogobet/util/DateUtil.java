4b0848ecb8db90e7a8512c2b768fd75cbaa1a9ce
u6remix <pochtaremix@gmail.com>
2017-03-20 22:27:57 +0300

    ololo

--- collector-web/src/main/java/by/blogobet/util/DateUtil.java
+++ collector-web/src/main/java/by/blogobet/util/DateUtil.java
@@ -6,6 +6,7 @@ public class DateUtil {
 
 	//test 123
 	//test12345678
+	//ololo
 	public static Date getDate2HoursBefore(){
 		long time2HourBefore = new Date().getTime() - 2*60*60*1000;
 		Date date = new Date(time2HourBefore);
