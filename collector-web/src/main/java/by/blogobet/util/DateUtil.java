9be6ed4fc8f987b04673fcf4a8788a44ba2faf2f
u6remix <pochtaremix@gmail.com>
2017-03-20 22:27:16 +0300

    test 123456789

--- collector-web/src/main/java/by/blogobet/util/DateUtil.java
+++ collector-web/src/main/java/by/blogobet/util/DateUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 public class DateUtil {
 
 	//test 123
+	//test12345678
 	public static Date getDate2HoursBefore(){
 		long time2HourBefore = new Date().getTime() - 2*60*60*1000;
 		Date date = new Date(time2HourBefore);
